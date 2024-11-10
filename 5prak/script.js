47cb6581c38cd6349c3cceb4e09f3d992c3777c5
MaximShip <72982786+MaximShip@users.noreply.github.com>
2024-11-10 14:46:41 +0300

    Add files via upload

---
+++ 5prak/script.js
@@ -0,0 +1,24 @@
+const main = document.querySelector(".main")
+
+
+
+function open_form(){
+    main.style.display = "block";
+}
+
+function close_form(){
+    main.style.display = "none";
+}
+
+function confirm_order(type) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const number = document.getElementById('number').value;
+    const date = document.getElementById('date').value;
+    
+    if (!(name && email && number && date)) {
+        alert("Не все поля заполнены!");
+    } else {
+        alert("Все поля заполнены! Услгуа ");
+    }
+}
